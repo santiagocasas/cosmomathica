38e7623689c6cda475e0b7f7f78a6b62b304c5d4
Santiago Casas <casas@thphys.uni-heidelberg.de>
2015-11-27 01:10:27 +0100

    Added a line to download.sh to allow download of power.c for Eisenstein&Hu Modified mathlink name added c sources into tf

--- ext/download.sh
+++ ext/download.sh
@@ -3,6 +3,8 @@
 echo "Downloading Eisenstein & Hu's transfer function code..."
 mkdir -p tf
 wget http://background.uchicago.edu/~whu/transfer/tf_fit.c -q -O tf/tf_fit.c
+echo "Downloading Eisenstein & Hu's power spectra code..."
+wget http://background.uchicago.edu/~whu/transfer/power.c -q -O tf/power.c
 echo "Downloading Robert E. Smith's et al. halofit code..."
 mkdir -p halofit
 wget http://www.roe.ac.uk/~jap/haloes/halofit+.tar -q -O - | tar x -C halofit
